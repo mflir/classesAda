999ff40c2cb588fb65941e374ab8ce3d7baead96
Florencia Scavone <scavone_f@hotmail.com>
2017-10-25 08:45:23 -0300

    nuevas practicas

---
+++ funcionesJS/disfraz.js
@@ -0,0 +1,5 @@
+function randomHalloween(disfraces){
+	var posicion = Math.floor((Math.random() * disfraces.lemgth-1 ));
+	return disfraces[posicion];
+}
+
---
+++ funcionesJS/ej1.js
@@ -0,0 +1,13 @@
+function funcion (base, exponente){
+	var resultado = 1;
+	for(i=0; i<=exponente; i++){
+		//0<0 = no? no entra ---> nuestro resultado 
+		resultado = resultado * base:
+	}
+	return resultado;
+}
+
+var operacion = potencia (5,2);
+
+console.log (operacion);
+
